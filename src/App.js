04ed85a13ea898bf5d1b89f113a9ab90b2b59f2e
Yaren <yarenmertt96@gmail.com>
2023-02-07 01:54:41 +0300

    proje esnek görevlerle beraber tamamlandı.

--- src/App.js
+++ src/App.js
@@ -6,29 +6,63 @@ import { data } from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+//contextler
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(initialStateOku("cart"));
+
+  function cartLocalStorageYaz(cartParam) {
+    localStorage.setItem("cart", JSON.stringify(cartParam));
+  }
+
+  function cartLocalStorageOku(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  function initialStateOku(key) {
+    const initialCart = cartLocalStorageOku(key);
+
+    if (initialCart) {
+      return initialCart;
+    } else {
+      return [];
+    }
+  }
 
   const addItem = (item) => {
+    const newCart = [...cart, item];
+    setCart(newCart);
+
     // verilen itemi sepete ekleyin
+    cartLocalStorageYaz(newCart);
+  };
+
+  const removeItem = (id) => {
+    const newCart = [...cart.filter((c) => c.id !== id)];
+    setCart(newCart);
+    cartLocalStorageYaz(newCart);
   };
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart, removeItem }}>
+          <Navigation /*cart={cart}*/ />
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
+            <Route path="/cart">
+              <ShoppingCart /*cart={//cart}*/ />
+            </Route>
+          </main>
+        </CartContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
 import { ScNav } from "./scParts";
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const { cart } = useContext(CartContext);
   return (
     <ScNav>
       <nav className="content">
         <NavLink to="/">Products</NavLink>
         <NavLink to="/cart">
-          Cart <span>{props.cart.length}</span>
+          Cart <span>{cart.length}</span>
         </NavLink>
       </nav>
     </ScNav>
